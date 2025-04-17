262065360e2b43bf7c0d01b90c24fe09c3ffcbeb
FE4902 <isdn000123@gmail.com>
2025-04-17 23:29:46 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <h1>Vue3 Job</h1>
+    <p>text color</p>
+    <a href="#">link color</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  color: red;
+
+  &:hover {
+    color: blue;
+  }
+}
+</style>
