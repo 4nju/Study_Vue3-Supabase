72bd1d5fedeec4883aa39c36fbc3327c3aa2cf5e
4nju <isdn000123@gmail.com>
2025-04-19 11:21:20 +0900

    회원가입 페이지

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,7 +10,6 @@ const handleLogin = () => {
 </script>
 
 <template>
-  <h1>Login</h1>
   <div class="form-container">
     <form @submit.prevent="handleLogin">
       <div class="form-group">
@@ -40,44 +39,5 @@ const handleLogin = () => {
 </template>
 
 <style lang="scss" scoped>
-.form-container {
-  max-width: 768px;
-  margin: 0 auto;
-  padding: 15px;
-  margin-top: 80px;
-  background-color: #fff;
-}
-
-.form-group {
-  margin-bottom: 24px;
-}
-
-label {
-  display: block;
-  margin-bottom: 5px;
-  font-weight: bold;
-}
-
-input {
-  width: 100%;
-  padding: 10px;
-  box-sizing: border-box;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-
-button {
-  width: 100%;
-  padding: 10px;
-  background-color: var(--main-color-dark);
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin-bottom: 24px;
-}
-
-button:hover {
-  opacity: 0.8;
-}
+@use "../styles/form.scss";
 </style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,7 +1,58 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+const tel = ref("");
+const text = ref("");
+
+const handleSignup = () => {
+  console.log(email.value, password.value, tel.value, text.value);
+};
+</script>
 
 <template>
-  <h1>Sign Up</h1>
+  <div class="form-container">
+    <form @submit.prevent="handleSignup">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          id="email"
+          v-model="email"
+          type="text"
+          placeholder="이메일 입력"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          id="password"
+          v-model="password"
+          type="password"
+          placeholder="비밀번호 입력"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="tel">Phone</label>
+        <input
+          id="tel"
+          v-model="tel"
+          type="tel"
+          placeholder="전화번호 입력"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="text">자기소개</label>
+        <textarea id="text" v-model="text" />
+      </div>
+      <button type="submit">회원가입</button>
+    </form>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../styles/form.scss";
+</style>
